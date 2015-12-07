c628d578429aebec3ca163d4e5a69ff00d7bb59e
Yuriy Botsev <YurBoc@yandex.ru>
2015-12-07 18:14:35 +0300

    Add fifo (from server to client) for state notifications

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 
 const char *fifoFileName = "/tmp/led_fifo";
+const char *fifoStateFileName = "/tmp/state_fifo";
 
 CmdResult performCommand(Led *led, const string& cmd, const string& arg)
 {
@@ -38,14 +39,21 @@ int main()
 
   cout << "[SYS] Preparing server..." << endl;
   int ledFifo = mkfifo(fifoFileName, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
+  int stateFifo = mkfifo(fifoStateFileName, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
   if (ledFifo != 0) {
     cerr << "[SYS] Can't create FIFO. Check if another example of server already running." << endl;
     cerr << "[SYS] If another server is not running perform 'rm " << fifoFileName << "' from console." << endl;
     return 1;
   }
+  if (stateFifo != 0) {
+    cerr << "[SYS] Can't create state FIFO. Check if another example of server already running." << endl;
+    cerr << "[SYS] If another server is not running perform 'rm " << fifoStateFileName << "' from console." << endl;
+    return 1;
+  }
 
   cout << "[SYS] Ready for commands." << endl;
   ifstream fifo(fifoFileName);
+  ofstream ledState(fifoStateFileName);
   string line;
   while (true)
   {
@@ -63,11 +71,13 @@ int main()
       cout << "[CMD] \"" << cmd << "\": \"" << arg << "\"" << endl;
       cout << commandState << endl;
 #endif
+      ledState << commandState << endl;
     }
   }
 
   cout << "[SYS] Server shutdown." << endl;
   fifo.close();
   unlink(fifoFileName);
+  unlink(fifoStateFileName);
   return 0;
 }
